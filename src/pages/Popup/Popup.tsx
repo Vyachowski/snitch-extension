09ab7f72cf376c7af32a3797b9d128d3e9bb61ad
Slava Haikin <vyachowski@gmail.com>
2025-10-04 20:09:18 +0300

    feat: add fingerprint lib

---
+++ src/pages/Popup/Popup.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import logo from '../../assets/img/logo.svg';
+import { ThumbmarkProvider, useThumbmark } from '@thumbmarkjs/react';
+import './Popup.css';
+
+const Popup = () => {
+  const { thumbmark, visitorId, isLoading } = useThumbmark();
+
+  return (
+    <ThumbmarkProvider>
+      {!isLoading && <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>{visitorId}</code> and save to {thumbmark}.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React!
+          </a>
+        </header>
+        <a href="https://www.flaticon.com/free-icons/status" title="status icons">Status icons created by Karacis - Flaticon</a>
+      </div>}
+    </ThumbmarkProvider>
+  );
+};
+
+export default Popup;
